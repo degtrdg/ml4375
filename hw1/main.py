bc3ae756bde71ea06c997f8cb891b47e46e13492
Daniel George <degtrdg@github.com>
2023-09-05 16:27:04 -0500

    boilerplate and reading in df

---
+++ hw1/main.py
@@ -0,0 +1,9 @@
+import sys
+
+def main(train_file, test_file):
+    # Your code here
+
+if __name__ == "__main__":
+    train_file = sys.argv[1]
+    test_file = sys.argv[2]
+    main(train_file, test_file)
